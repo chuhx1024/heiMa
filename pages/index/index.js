087285d4d66554951dc8b1ad854e8ff6dd5225b8
chuhx <chuhx@yonyou.com>
2019-12-25 16:49:01 +0800

    首页轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,23 @@
 //Page Object
 Page({
   data: {
+    // 轮播图数组
+    swiperList: []
     
   },
   //options(Object)
   onLoad: function(options) {
+    // 发送异步请求  获取轮播图的数据
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          swiperList: res.data.message
+        })
+      }
+    });
+      
     
   },
   onReady: function() {
