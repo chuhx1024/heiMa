94428f8068a99309269e439ca0f8b64f731adb58
chuwx <var_chx@126.com>
2019-12-25 19:23:37 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,29 +4,44 @@ import { request} from "../../request/index.js"
 Page({
   data: {
     // 轮播图数组
-    swiperList: []
-    
+    swiperList: [],
+    // 导航数组
+    cateList: [],
+    // 楼层数据
+    floorList: [] 
   },
   //options(Object)
   onLoad: function(options) {
-    // 发送异步请求  获取轮播图的数据
-    // wx.request({
-    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-    //   success: (res) => {
-    //     console.log(res)
-    //     this.setData({
-    //       swiperList: res.data.message
-    //     })
-    //   }
-    // });
+    this.getSwiperList()
+    this.getCateList()
+    this.getFloorList()
+  },
+  // 发送异步请求  获取轮播图的数据
+  getSwiperList () {
     request({ url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata'})
     .then( res => {
       this.setData({
         swiperList: res.data.message
       })
     })
-      
-    
+  },
+  // 发送异步请求  获取分类导航的数据
+  getCateList () {
+    request({ url: 'https://api.zbztb.cn/api/public/v1/home/catitems'})
+    .then( res => {
+      this.setData({
+        cateList: res.data.message
+      })
+    })
+  },
+  // 发送异步请求  获取楼层的数据
+  getFloorList () {
+    request({ url: 'https://api.zbztb.cn/api/public/v1/home/floordata'})
+    .then( res => {
+      this.setData({
+        floorList: res.data.message
+      })
+    })
   },
   onReady: function() {
     
