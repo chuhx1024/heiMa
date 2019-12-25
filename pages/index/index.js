f981be34883f882fcc2c9a7d479a57c68568969c
chuhx <chuhx@yonyou.com>
2019-12-25 17:09:27 +0800

    封装wx的request 为Promise

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,6 @@
 //Page Object
+// 引入用来发送请求的方法
+import { request} from "../../request/index.js"
 Page({
   data: {
     // 轮播图数组
@@ -8,15 +10,21 @@ Page({
   //options(Object)
   onLoad: function(options) {
     // 发送异步请求  获取轮播图的数据
-    wx.request({
-      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          swiperList: res.data.message
-        })
-      }
-    });
+    // wx.request({
+    //   url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+    //   success: (res) => {
+    //     console.log(res)
+    //     this.setData({
+    //       swiperList: res.data.message
+    //     })
+    //   }
+    // });
+    request({ url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata'})
+    .then( res => {
+      this.setData({
+        swiperList: res.data.message
+      })
+    })
       
     
   },
