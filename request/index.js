be8ff3c85ae21693f4a8928db0442854c6ec0702
chuhx <chuhx@yonyou.com>
2019-12-26 20:43:27 +0800

    优化 ajax  所有请求都回来了  才关闭 loading 效果  思路很好 值得借鉴

--- request/index.js
+++ request/index.js
@@ -1,4 +1,7 @@
+let ajaxTimes = 0 
 export const request = (params) => {
+  // 每次调用都 ++ 统计次数
+  ajaxTimes ++ 
   wx.showLoading({
     title: '加载中',
     mask: true
@@ -13,7 +16,13 @@ export const request = (params) => {
         reject(err)
       },
       complete: () => {
-        wx.hideLoading()
+        // 完成一个就减 1
+        // 能成功的原因是要领会 export 的 作用范围  
+        ajaxTimes --
+        console.log(ajaxTimes, 123)
+        if (ajaxTimes === 0) {
+          wx.hideLoading()
+        }
       }
     })
   })
