21ad896f9ffe599a66efb57e31e88564a06d5ed8
chuhx <chuhx@yonyou.com>
2019-12-28 11:32:59 +0800

    使用  try catch  处理异常

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -18,16 +18,20 @@ Page({
   },
   // 获取用户的收货地址
   async handleChooseAddress () {
-    // 获取用户的权限
-    let res1 = await getSetting()
-    const scopeAddress = res1.authSetting['scope.address']
-    if (scopeAddress === true || scopeAddress === undefined) {
-      let res2 = await chooseAddress()
-      console.log(res2)
-    } else { // 没有权限 就诱导用户打开授权页面
-      await openSetting()
-      let res2 = await chooseAddress()
-      console.log(res2)
+    try {
+      // 获取用户的权限
+      let res1 = await getSetting()
+      const scopeAddress = res1.authSetting['scope.address']
+      if (scopeAddress === true || scopeAddress === undefined) {
+        let res2 = await chooseAddress()
+        console.log(res2)
+      } else { // 没有权限 就诱导用户打开授权页面
+        await openSetting()
+        let res2 = await chooseAddress()
+        console.log(res2)
+      }
+    } catch (err) {
+      console.log(err)
     }
   },
 
