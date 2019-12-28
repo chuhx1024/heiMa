77f27c671d1f2bcc11d87c76e153ce331c48e4eb
chuhx <chuhx@yonyou.com>
2019-12-28 16:02:57 +0800

    购物车静态页完成

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -7,14 +7,18 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 收货地址的数据
+    address: {}
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow () {
+    // 获取缓存中的购物车数据
+    const address = wx.getStorageSync('address');
+    address.all = address.provinceName + address.cityName + address.countyName + address.detailInfo
+    this.setData({
+      address
+    })
+      
   },
   // 获取用户的收货地址
   async handleChooseAddress () {
@@ -22,65 +26,17 @@ Page({
       // 获取用户的权限
       let res1 = await getSetting()
       const scopeAddress = res1.authSetting['scope.address']
+      let address = {}
       if (scopeAddress === true || scopeAddress === undefined) {
-        let res2 = await chooseAddress()
-        console.log(res2)
+        address = await chooseAddress()
       } else { // 没有权限 就诱导用户打开授权页面
         await openSetting()
-        let res2 = await chooseAddress()
-        console.log(res2)
+        address = await chooseAddress()
       }
+      // 获取收货地址后把其存储到本地存储
+      wx.setStorageSync('address', address)
     } catch (err) {
       console.log(err)
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
